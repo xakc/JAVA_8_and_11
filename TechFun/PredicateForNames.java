d892888a3bfb171fd1ad39c178353d9428f59e69
xakc <55619126+xakc@users.noreply.github.com>
2022-07-07 14:14:29 +0300

    Predicate for name length

---
+++ TechFun/PredicateForNames.java
@@ -0,0 +1,22 @@
+package com.n3.FunctionaPrograming;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Predicate;
+
+public class PredicateForNames {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Predicate<String> validLength = name -> name.length() <= n ;
+        String[] names = scanner.nextLine().split("\\s+");
+
+        Arrays.stream(names).filter(validLength)
+                .forEach(System.out::println);
+
+
+    }
+}
