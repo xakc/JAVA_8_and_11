5da4feed74fa006d03828e09dd7786fe489f3f8e
xakc <55619126+xakc@users.noreply.github.com>
2022-07-12 22:42:41 +0300

    Refactor Bank Account

--- BankAccount.java
+++ BankAccount.java
@@ -1,4 +1,17 @@
 package com.n3.ObjectsAndClasses;
 
 public class BankAccount {
+    private static int accountCaunt = 1;
+    private static double interestRate = 0.02 ;
+
+
+    private int id;
+    private double balance;
+
+    public BankAccount() {
+        this.id = accountCaunt;
+        accountCaunt++;
+
+        
+    }
 }
