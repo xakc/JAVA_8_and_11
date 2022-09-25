4c783e38486e267a5a4cc46018a7cc26e1797369
xakc <55619126+xakc@users.noreply.github.com>
2022-09-25 22:16:04 +0300

    Adding Car interface

---
+++ interfacesandabstraction/Car.java
@@ -0,0 +1,11 @@
+package com.n3.interfacesandabstraction;
+
+public interface Car {
+    public static final  int TIRES = 4;
+
+    public String getModel();
+    public String getColor();
+    public String getHorsePower();
+
+
+}
