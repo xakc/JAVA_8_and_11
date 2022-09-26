aa47cb4d7d645f1db67602a3ff74d997a9fd0450
xakc <55619126+xakc@users.noreply.github.com>
2022-09-26 09:37:52 +0300

    Ading Macedonian Class

---
+++ sayhello/Macedonian.java
@@ -0,0 +1,20 @@
+package com.n3.sayhello;
+
+public class Macedonian implements Person
+{
+    private String name;
+
+    public Macedonian(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getHello() {
+        return null;
+    }
+}
