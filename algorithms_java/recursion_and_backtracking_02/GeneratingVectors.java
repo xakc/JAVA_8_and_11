0c477bd32e50d87bfd9818726d80bda78080ab6b
xakc <55619126+xakc@users.noreply.github.com>
2022-10-29 18:44:52 +0300

    Generating Vectors demo code
    Generating Vectors demo code added

---
+++ algorithms_java/recursion_and_backtracking_02/GeneratingVectors.java
@@ -0,0 +1,33 @@
+package com.n3.algorithms_java.recursion_and_backtracking_02;
+import java.util.Scanner;
+public class GeneratingVectors {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Integer[] memory = new Integer[n];
+
+        fillVector(memory, 0);
+    }
+
+    private static void fillVector(Integer[] memory, int index) {
+        if (index >= memory.length) {
+            print(memory);
+            return;
+        }
+
+        for (int i = 0; i <= 1; i++) {
+            memory[index] = i;
+            fillVector(memory, index + 1);
+        }
+
+    }
+
+    private static void print(Integer[] memory) {
+        for (Integer integer : memory) {
+            System.out.print(integer);
+        }
+        System.out.println();
+    }
+}
