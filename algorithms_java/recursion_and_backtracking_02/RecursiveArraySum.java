11f8d9e82c4b780647bb09fec0659f8e0d97eb48
xakc <55619126+xakc@users.noreply.github.com>
2022-10-30 18:43:05 +0200

    Recursive Array Sum Java demo code
    RecursiveArraySum java demo code added

---
+++ algorithms_java/recursion_and_backtracking_02/RecursiveArraySum.java
@@ -0,0 +1,35 @@
+package com.n3.algorithms_java.recursion_and_backtracking_02;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class RecursiveArraySum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] arr = Arrays.stream(scanner.nextLine()
+                        .split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+
+        System.out.println("Iteration sum: " + sum);
+
+        int recursionSum = sumNumbers(arr, 0);
+
+        System.out.println("Recursion sum: " + recursionSum);
+    }
+
+    private static int sumNumbers(int[] numbers, int index) {
+
+        if (index >= numbers.length) {
+            return 0;
+        }
+
+        return numbers[index] + sumNumbers(numbers, index + 1);
+    }
+}
