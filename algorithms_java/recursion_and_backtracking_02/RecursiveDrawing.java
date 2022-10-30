4d78d757b0e830ca1134ef185d45778eeb09c841
xakc <55619126+xakc@users.noreply.github.com>
2022-10-30 18:44:22 +0200

    Recursive Drawing Java demo code
    RecursiveDrawing java demo code added

---
+++ algorithms_java/recursion_and_backtracking_02/RecursiveDrawing.java
@@ -0,0 +1,32 @@
+package com.n3.algorithms_java.recursion_and_backtracking_02;
+import java.util.Scanner;
+
+public class RecursiveDrawing {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        drawFigure(n);
+    }
+
+    private static void drawFigure(int n) {
+        if (n == 0) {
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("*");
+        }
+
+        System.out.println();
+
+        drawFigure(n - 1);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("#");
+        }
+
+        System.out.println();
+    }
+}
