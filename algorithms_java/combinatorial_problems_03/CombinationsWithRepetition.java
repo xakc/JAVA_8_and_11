3fbc5261c0e45a22fe438ff4984ba3539ce6acd6
xakc <55619126+xakc@users.noreply.github.com>
2022-10-31 06:27:43 +0200

    CombinationsWithRepetition
    CombinationsWithRepetition added

---
+++ algorithms_java/combinatorial_problems_03/CombinationsWithRepetition.java
@@ -0,0 +1,32 @@
+package com.n3.combinatorial_problems_03;
+import java.util.Scanner;
+
+public class CombinationsWithRepetition {
+
+    public static String[] elements;
+    public static String[] combinations;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        elements = scanner.nextLine().split("\\s+");
+        int k = Integer.parseInt(scanner.nextLine());
+        combinations = new String[k];
+
+        combinations(0, 0);
+    }
+
+    private static void combinations(int index, int start) {
+        if (index == combinations.length) {
+            print(combinations);
+        } else {
+            for (int i = start; i < elements.length; i++) {
+                combinations[index] = elements[i];
+                combinations(index + 1, i);
+            }
+        }
+    }
+
+    private static void print(String[] arr) {
+        System.out.println(String.join(" ", arr));
+    }
+}
