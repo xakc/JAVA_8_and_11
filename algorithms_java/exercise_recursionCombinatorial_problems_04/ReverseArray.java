bb7e7b978f3c3a7f186d5a6e1e24d7a665f861d4
xakc <55619126+xakc@users.noreply.github.com>
2022-11-08 09:49:21 +0200

    Reverse an Array
    ReverseArray added

---
+++ algorithms_java/exercise_recursionCombinatorial_problems_04/ReverseArray.java
@@ -0,0 +1,21 @@
+package com.n3.exercise_recursionCombinatorial_problems_04;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ReverseArray {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String[] elements = bufferedReader.readLine().split("\\s+");
+
+        printReversedArray(elements, elements.length - 1);
+    }
+
+    private static void printReversedArray(String[] elements, int index) {
+        if (index < 0) {
+            return;
+        }
+        System.out.print(elements[index] + " ");
+        printReversedArray(elements, index - 1);
+    }
+}
