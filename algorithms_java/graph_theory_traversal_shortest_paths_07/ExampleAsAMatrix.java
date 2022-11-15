dc91cd54e73a52851427484efec4083b106e9258
xakc <55619126+xakc@users.noreply.github.com>
2022-11-15 09:52:19 +0200

    Example As A Matrix Java Algo
    ExampleAsAMatrix added

---
+++ algorithms_java/graph_theory_traversal_shortest_paths_07/ExampleAsAMatrix.java
@@ -0,0 +1,11 @@
+package com.n3.graph_theory_traversal_shortest_paths_07;
+
+public class ExampleAsAMatrix {
+    public static void main(String[] args) {
+        int nodes = 10;
+        int[][] graph = new int[nodes + 1][nodes + 1];
+
+        // 3 - > 6
+        graph[3][6] = 1;
+    }
+}
