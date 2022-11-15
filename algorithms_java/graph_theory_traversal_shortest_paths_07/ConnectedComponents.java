b94bc75d917a2a0ed9a0196a5a9afe4d1c75691a
xakc <55619126+xakc@users.noreply.github.com>
2022-11-15 09:49:47 +0200

    Graph Algos
    graf algo and package added.

---
+++ algorithms_java/graph_theory_traversal_shortest_paths_07/ConnectedComponents.java
@@ -0,0 +1,80 @@
+package com.n3.graph_theory_traversal_shortest_paths_07;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ConnectedComponents {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String nextLine = scanner.nextLine();
+            if (nextLine.trim().equals("")) {
+                graph.add(new ArrayList<>());
+            } else {
+                List<Integer> nextNodes = Arrays.stream(nextLine.split("\\s+"))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
+
+                graph.add(nextNodes);
+            }
+        }
+
+        List<Deque<Integer>> connectedComponents = getConnectedComponents(graph);
+
+        for (Deque<Integer> connectedComponent : connectedComponents) {
+            System.out.print("Connected component: ");
+            for (int integer : connectedComponent) {
+                System.out.print(integer + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static List<Deque<Integer>> getConnectedComponents(List<List<Integer>> graph) {
+        boolean[] visited = new boolean[graph.size()];
+        List<Deque<Integer>> components = new ArrayList<>();
+
+        for (int start = 0; start < graph.size(); start++) {
+            if (!visited[start]) {
+                components.add(new ArrayDeque<>());
+                bfs(start, components, graph, visited);
+            }
+        }
+        return components;
+    }
+
+    private static void bfs(int start, List<Deque<Integer>> components, List<List<Integer>> graph, boolean[] visited) {
+        Deque<Integer> queue = new ArrayDeque<>();
+        visited[start] = true;
+        queue.push(start);
+
+        while (!queue.isEmpty()) {
+            int node = queue.pop();
+
+            components.get(components.size() - 1).offer(node);
+
+            for (int child : graph.get(node)) {
+                if (!visited[child]) {
+                    visited[child] = true;
+                    queue.push(child);
+                }
+            }
+        }
+    }
+
+    private static void dfs(int node, List<Deque<Integer>> components, List<List<Integer>> graph, boolean[] visited) {
+        if (!visited[node]) {
+            visited[node] = true;
+
+            for (int child : graph.get(node)) {
+                dfs(child, components, graph, visited);
+            }
+            components.get(components.size() - 1).offer(node);
+        }
+    }
+
+}
